f07dab48aaf4072001c2c275c1d4f5eb20b9925e
rptejas@gmail.com <Ingaman01*>
2020-12-24 15:58:42 +0300

    Creating Resume Map with basic resume information.

---
+++ src/Components/resume.js
@@ -0,0 +1,33 @@
+let resume = {
+    "imageUrl" : "",
+    "name" : "Tejas Pradeep",
+    "role" : "Junior at Georgia Tech, BSCS",
+    "linkedIn_ID" : "tejas-pradeep",
+    "aboutMe" : "",
+    "socials" : [{
+        "name" : "linkedin",
+        "url" : "https://www.linkedin.com/in/tejas-pradeep",
+        "className" : "fab fa-linkedin"
+    },
+    {
+        "name" : "github",
+        "url" : "http://www.github.com/tejas-pradeep",
+        "className" : "fab fa-github"
+
+    }],
+    "address" : "Atlanta, GA",
+    "website" : "",
+    "education" : [
+        {
+            "University" : "Georgia Institute of Technology",
+            "Degree" : "Bachelor's of Science",
+            "Major" : "Computer Science",
+            "Threads" : "Intelligence and Information Info-Networks",
+            "GradMonth" : "May",
+            "GradYear" : "2022",
+            "GPA" : "3.92",
+            "Major GPA" : "4.0"
+        }
+    ]
+}
+export default resume;
