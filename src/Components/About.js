05eeaf2698b2458bb582c3d48219fb61a34981f4
rptejas@gmail.com <Ingaman01*>
2020-12-24 16:20:14 +0300

    Adding in HTML for the About.js card.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,28 @@
 import React, {Component} from "react";
 export default class About extends Component {
     render() {
+        let resumeData = this.props.resumeData;
         return (
-            <section id={'about-section'}>
-                <div className={'row'}>
-                    <div className={'col-2'}>
-
+            <section id={"about"}>
+                <div className={"row"}>
+                    <div className={"three columns"}>
+                        <img className={"profile-pic"} src = "images/tejas_pradeep_profile_pic.jpg" alt=""/>
                     </div>
-                    <div className={'col-2'}>
-
+                    <div className={"nine columns main-col"}>
+                        <h2>About Me!</h2>
+                        <p> {resumeData.aboutMe}</p>
+                        <div className={"row"}>
+                            <div className={"columns contact-details"}>
+                                <h2>Contact Details</h2>
+                                <p className={"address"}>
+                                    <span>{resumeData.name}</span>
+                                    <br/>
+                                    <span>{resumeData.address}</span>
+                                    <br/>
+                                    <span>{resumeData.website}</span>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
