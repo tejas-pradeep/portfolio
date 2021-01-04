2546972ef9a68d123e04ccf857616d2c9c1d05d4
rptejas@gmail.com <rptejas@gatech.edu>
2021-01-04 22:04:35 +0300

    Adding HTML for the contacts section

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,24 +1,28 @@
 import React, {Component} from "react";
 export default class Contact extends Component {
     render() {
-        let resumeData = this.props.resumeData;
+        const cards = document.querySelectorAll('.card');
+
         return (
             <section id={"contact"}>
                 <div className={"row section-head"}>
-                    <div className={"ten column"}>
-                        <p className={"lead"}>
-                            Feel free to contact me regarding any job opportunities or suggestions below.
-                        </p>
-                    </div>
+                    <h1><span>Contact Me!</span></h1>
                 </div>
-                <div className={"row"}>
-                    <aside className={"eight columns footer-widgets"}>
-                        <div className={"widget"}>
-                            <h4>LinkedIn : {resumeData.linkedIn_ID}</h4>
-                            <br/>
-                            <h4>Email : {resumeData.email}</h4>
-                        </div>
-                    </aside>
+                <div className="contact-info">
+                    <div className="card" redirect-link={"mailto:rptejas@gatech.edu"}>
+                        <i className="card-icon far fa-envelope"></i>
+                        <p>rptejas@gatech.edu</p>
+                    </div>
+
+                    <div className="card">
+                        <i className="card-icon fas fa-phone"></i>
+                        <p href={"tel:512-665-0719"}>+1 512-665-0719</p>
+                    </div>
+
+                    <div className="card">
+                        <i className="card-icon fab fa-linkedin"></i>
+                        <p href={"https://www.linkedin.com/in/tejas-pradeep/"}>linkedin.com/in/tejas-pradeep</p>
+                    </div>
                 </div>
             </section>
         )
