c73f5e9f34faec6b7bc8a9d6b9ab061485df1240
rptejas@gmail.com <rptejas@gatech.edu>
2021-01-12 17:32:25 +0300

    Adding package for scroll header current highlighting

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import ActiveMenuLink from "active-menu-link";
 import Header from "./Components/Header";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Skills from "./Components/Skills";
 import Timeline from "./Components/Timeline";
 import Work from "./Components/Work";
+import Footer from "./Components/Footer";
 import resume from "./Components/resume";
 import './App.css';
 import "./css/default.css";
 import "./css/layout.css";
 import "./css/media-queries.css";
-import Footer from "./Components/Footer";
+
 
 
 function App() {
+    React.useEffect(() => {
+        new ActiveMenuLink("#nav-wrap", {activeClass : "current", ease:"inQuad"});
+    }, []);
   return (
       <div className={"App"}>
           <Header resumeData={resume}/>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import ActiveMenuLink from "active-menu-link";
 
 export default class Header extends Component {
    render () {
@@ -16,9 +17,10 @@ export default class Header extends Component {
                      <a className={"mobile-btn"} href={"#"} title={"Hide menu"}>Hide menu</a>
                      <ul id={"nav"} className={"nav"}>
                          <li onClick={handleCurrent}><a className={"current"} href={"#home"}>Home</a></li>
+                         <li onClick={handleCurrent}><a href={"#about"}>About Me</a></li>
                          <li onClick={handleCurrent}><a href={"#skills"}>Skills</a></li>
+                         <li onClick={handleCurrent}><a href={"#work"}>Work Experience</a></li>
                          <li onClick={handleCurrent}><a href={"#timeline"}>Timeline</a></li>
-                         <li onClick={handleCurrent}><a href={"#projects"}>Projects!</a></li>
                          <li onClick={handleCurrent}><a href={"#contact"}>Contact Me!</a></li>
                          <li onClick={handleCurrent}><a href={process.env.PUBLIC_URL + "/Downloads/Tejas_Pradeep_Resume.pdf"} download={"Tejas_Pradeep_Resume.pdf"}>Download Resume <i className={"fas fa-download"}/></a></li>
                      </ul>
